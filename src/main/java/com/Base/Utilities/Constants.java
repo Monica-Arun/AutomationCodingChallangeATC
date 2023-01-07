4d235ec232d7070a19254219f1862c68bb805edf
Monica_Arun <monicasri17@gmail.com>
2023-01-07 11:36:50 +0530

    Automation fix

---
+++ src/main/java/com/Base/Utilities/Constants.java
@@ -0,0 +1,16 @@
+package com.Base.Utilities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Constants {
+
+	public static final String CHROME = "Chrome";
+	public static final String FIREFOX = "Firefox";
+	public static final String SAFARI = "safari";
+	public static final String IE = "IE";
+	public static final int default_Timeout=20;
+	public static final int pageload_Timeout=100;
+	public static final int script_Timeout=100;
+
+}
